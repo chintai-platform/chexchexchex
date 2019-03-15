4ea6918a6f1afbc0c0d3cf15f29a325efe7a3049
PhillipHamnett <phillip.hamnett@gmail.com>
2019-03-15 09:28:46 +0100

    Updated code to contain locking and unlocking functions

--- include/chex.hpp
+++ include/chex.hpp
@@ -6,110 +6,107 @@
 
 #include <eosiolib/asset.hpp>
 #include <eosiolib/eosio.hpp>
-#include <eosiolib/time.hpp>
 
 #include <string>
 
-namespace chintai
-{
+namespace eosiosystem {
+   class system_contract;
+}
+
+namespace eosio {
 
    using std::string;
 
-   class [[eosio::contract("chexchexchex")]] token : public eosio::contract {
+   class [[eosio::contract("eosio.token")]] token : public contract {
       public:
          using contract::contract;
 
          [[eosio::action]]
-         void create( eosio::name   issuer,
-                      eosio::asset  maximum_supply);
+         void create( name   issuer,
+                      asset  maximum_supply);
 
          [[eosio::action]]
-         void issue( eosio::name to, eosio::asset quantity, string memo );
+         void issue( name to, asset quantity, string memo );
 
          [[eosio::action]]
-         void retire( eosio::asset quantity, string memo );
+         void retire( asset quantity, string memo );
 
          [[eosio::action]]
-         void burn( eosio::name owner, eosio::asset quantity, string memo );
-
-         [[eosio::action]]
-         void stake( eosio::name owner, eosio::asset quantity );
-
-         [[eosio::action]]
-         void unstake( eosio::name owner, eosio::asset quantity );
-
-         [[eosio::action]]
-         void transfer( eosio::name    from,
-                        eosio::name    to,
-                        eosio::asset   quantity,
+         void transfer( name    from,
+                        name    to,
+                        asset   quantity,
                         string  memo );
 
          [[eosio::action]]
-         void open( eosio::name owner, eosio::symbol const & symbol, eosio::name ram_payer );
+         void open( name owner, const symbol& symbol, name ram_payer );
 
          [[eosio::action]]
-         void close( eosio::name owner, eosio::symbol const & symbol );
+         void close( name owner, const symbol& symbol );
 
-         static eosio::asset get_supply( eosio::name token_contract_account, eosio::symbol_code sym_code )
+         [[eosio::action]]
+         void lock( name owner, asset quantity, uint64_t time );
+
+         [[eosio::action]]
+         void unlock( name owner, asset quantity );
+
+         [[eosio::action]]
+         void burn( name owner, asset quantity );
+
+         static asset get_supply( name token_contract_account, symbol_code sym_code )
          {
             stats statstable( token_contract_account, sym_code.raw() );
             const auto& st = statstable.get( sym_code.raw() );
             return st.supply;
          }
 
-         static eosio::asset get_balance( eosio::name token_contract_account, eosio::name owner, eosio::symbol_code sym_code )
+         static asset get_balance( name token_contract_account, name owner, symbol_code sym_code )
          {
             accounts accountstable( token_contract_account, owner.value );
             const auto& ac = accountstable.get( sym_code.raw() );
             return ac.balance;
          }
 
+         using create_action = eosio::action_wrapper<"create"_n, &token::create>;
+         using issue_action = eosio::action_wrapper<"issue"_n, &token::issue>;
+         using retire_action = eosio::action_wrapper<"retire"_n, &token::retire>;
+         using transfer_action = eosio::action_wrapper<"transfer"_n, &token::transfer>;
+         using open_action = eosio::action_wrapper<"open"_n, &token::open>;
+         using close_action = eosio::action_wrapper<"close"_n, &token::close>;
       private:
          struct [[eosio::table]] account {
-            eosio::asset    balance;
-            eosio::asset    staked;
-            eosio::asset    unstaking;
-            account() : balance(0, eosio::symbol("CHEX",4)), staked(0, eosio::symbol("CHEX",4)), unstaking(0, eosio::symbol("CHEX",4)) {}
+            asset    balance;
+            asset    locked;
             uint64_t primary_key()const { return balance.symbol.code().raw(); }
-            EOSLIB_SERIALIZE( account, (balance)(staked)(unstaking) )
-         };
-
-         struct [[eosio::table]] staked {
-           eosio::time_point      staked_at;
-            eosio::asset    balance;
-            staked() : staked_at(eosio::microseconds(0)), balance(0, eosio::symbol("CHEX",4)) {}
-            uint64_t primary_key()const { return staked_at.sec_since_epoch(); }
-            EOSLIB_SERIALIZE( staked, (staked_at)(balance) )
-         };
-
-         struct [[eosio::table]] unstaked {
-           eosio::time_point      unstaked_at;
-           eosio::asset    balance;
-            unstaked() : unstaked_at(eosio::microseconds(0)), balance(0, eosio::symbol("CHEX",4)) {}
-
-            uint64_t primary_key()const { return unstaked_at.sec_since_epoch(); }
-            EOSLIB_SERIALIZE( unstaked, (unstaked_at)(balance) )
          };
 
          struct [[eosio::table]] currency_stats {
-            eosio::asset                   supply;
-            eosio::asset                   max_supply;
-            eosio::name                    issuer;
-            eosio::asset                   total_staked;
-            std::vector< eosio::asset >    total_staked_per_level;
+            asset    supply;
+            asset    max_supply;
+            name     issuer;
 
-            currency_stats() : supply(0, eosio::symbol("CHEX",4)), max_supply(0, eosio::symbol("CHEX",4)), issuer("chexchexchex"), total_staked(0, eosio::symbol("CHEX",4)) {eosio::print("Test\n");}
             uint64_t primary_key()const { return supply.symbol.code().raw(); }
-            EOSLIB_SERIALIZE( currency_stats, (supply)(max_supply)(issuer)(total_staked)(total_staked_per_level) )
+         };
+
+         struct [[eosio::table]] locked_fund {
+            uint64_t lock_time;
+            asset    quantity;
+            uint64_t primary_key()const { return lock_time; }
+         };
+
+         struct [[eosio::table]] unlocking_fund {
+            uint64_t id;
+            time_point unlocked_at;
+            asset    quantity;
+            uint64_t primary_key()const { return id; }
          };
 
          typedef eosio::multi_index< "accounts"_n, account > accounts;
          typedef eosio::multi_index< "stat"_n, currency_stats > stats;
-         typedef eosio::multi_index< "staked"_n, staked> staked_table;
-         typedef eosio::multi_index< "unstaked"_n, unstaked > unstaked_table;
+         typedef eosio::multi_index< "locked"_n, locked_fund > locked_funds;
+         typedef eosio::multi_index< "unlocking"_n, unlocking_fund > unlocking_funds;
 
-         void sub_balance( eosio::name owner, eosio::asset value );
-         void add_balance( eosio::name owner, eosio::asset value, eosio::name ram_payer );
+         void sub_balance( name owner, asset value );
+         void add_balance( name owner, asset value, name ram_payer );
    };
 
-} /// eosio::namespace eosio
+} /// namespace eosio
--- src/chex.cpp
+++ src/chex.cpp
@@ -3,178 +3,95 @@
  *  @copyright defined in eos/LICENSE.txt
  */
 
-#include <chex.hpp>
+#include <eosio.token/eosio.token.hpp>
 
-namespace chintai
+namespace eosio {
+
+void token::create( name   issuer,
+                    asset  maximum_supply )
 {
-
-  /*!
-    Create token
-  */
-  void token::create( eosio::name issuer, eosio::asset maximum_supply )
-  {
-    eosio::print("1\n");
     require_auth( _self );
-    eosio::print("2\n");
 
     auto sym = maximum_supply.symbol;
-    eosio::print("3\n");
-    eosio::check( sym.is_valid(), "invalid symbol name" );
-    eosio::print("4\n");
-    eosio::check( maximum_supply.is_valid(), "invalid supply");
-    eosio::print("5\n");
-    eosio::check( maximum_supply.amount > 0, "max-supply must be positive");
-    eosio::print("6\n");
+    check( sym.is_valid(), "invalid symbol name" );
+    check( maximum_supply.is_valid(), "invalid supply");
+    check( maximum_supply.amount > 0, "max-supply must be positive");
 
     stats statstable( _self, sym.code().raw() );
-    eosio::print("7\n");
     auto existing = statstable.find( sym.code().raw() );
-    eosio::print("8\n");
-    eosio::check( existing == statstable.end(), "token with symbol already exists" );
-    eosio::print("9\n");
+    check( existing == statstable.end(), "token with symbol already exists" );
 
     statstable.emplace( _self, [&]( auto& s ) {
-    eosio::print("10\n");
-        s.supply.symbol       = maximum_supply.symbol;
-    eosio::print("11\n");
-        s.max_supply          = maximum_supply;
-    eosio::print("12\n");
-        s.issuer              = issuer;
-    eosio::print("13\n");
-        s.total_staked.symbol = maximum_supply.symbol;
-    eosio::print("14\n");
-        for(int i = 0; i < 7; ++i)
-        {
-    eosio::print("15\n");
-          s.total_staked_per_level.push_back(s.supply);
-    eosio::print("16\n");
-        }
-    eosio::print("17\n");
-        });
-    eosio::print("18\n");
-  }
+       s.supply.symbol = maximum_supply.symbol;
+       s.max_supply    = maximum_supply;
+       s.issuer        = issuer;
+    });
+}
 
 
-  /*!
-    Issue token
-  */
-  void token::issue( eosio::name to, eosio::asset quantity, string memo )
-  {
-    eosio::print("19\n");
+void token::issue( name to, asset quantity, string memo )
+{
     auto sym = quantity.symbol;
-    eosio::print("20\n");
-    eosio::check( sym.is_valid(), "invalid symbol eosio::name" );
-    eosio::print("21\n");
-    eosio::check( memo.size() <= 256, "memo has more than 256 bytes" );
-    eosio::print("22\n");
+    check( sym.is_valid(), "invalid symbol name" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
 
     stats statstable( _self, sym.code().raw() );
-    eosio::print("23\n");
     auto existing = statstable.find( sym.code().raw() );
-    eosio::print("24\n");
-    eosio::check( existing != statstable.end(), "token with symbol does not exist, create token before issue" );
-    eosio::print("25\n");
+    check( existing != statstable.end(), "token with symbol does not exist, create token before issue" );
     const auto& st = *existing;
-    eosio::print("26\n");
 
     require_auth( st.issuer );
-    eosio::print("27\n");
-    eosio::check( quantity.is_valid(), "invalid quantity" );
-    eosio::print("28\n");
-    eosio::check( quantity.amount > 0, "must issue positive quantity" );
-    eosio::print("29\n");
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must issue positive quantity" );
 
-    eosio::check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
-    eosio::print("30\n");
-    eosio::check( quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
-    eosio::print("31\n");
+    check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    check( quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
 
-    statstable.modify( st, eosio::same_payer, [&]( auto& s ) {
-    eosio::print("32\n");
-        s.supply += quantity;
-    eosio::print("33\n");
-        });
+    statstable.modify( st, same_payer, [&]( auto& s ) {
+       s.supply += quantity;
+    });
 
-    eosio::print("34\n");
     add_balance( st.issuer, quantity, st.issuer );
-    eosio::print("35\n");
 
     if( to != st.issuer ) {
       SEND_INLINE_ACTION( *this, transfer, { {st.issuer, "active"_n} },
-          { st.issuer, to, quantity, memo }
-          );
+                          { st.issuer, to, quantity, memo }
+      );
     }
-    eosio::print("36\n");
-  }
+}
 
-  /*!
-    Retire token
-    Reduces the total quantity of tokens available by removing them from the issuer
-  */
-  void token::retire( eosio::asset quantity, string memo )
-  {
+void token::retire( asset quantity, string memo )
+{
     auto sym = quantity.symbol;
-    eosio::check( sym.is_valid(), "invalid symbol eosio::name" );
-    eosio::check( memo.size() <= 256, "memo has more than 256 bytes" );
+    check( sym.is_valid(), "invalid symbol name" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
 
     stats statstable( _self, sym.code().raw() );
     auto existing = statstable.find( sym.code().raw() );
-    eosio::check( existing != statstable.end(), "token with symbol does not exist" );
+    check( existing != statstable.end(), "token with symbol does not exist" );
     const auto& st = *existing;
 
     require_auth( st.issuer );
-    eosio::check( quantity.is_valid(), "invalid quantity" );
-    eosio::check( quantity.amount > 0, "must retire positive quantity" );
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must retire positive quantity" );
 
-    eosio::check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
 
-    statstable.modify( st, eosio::same_payer, [&]( auto& s ) {
-        s.supply -= quantity;
-        });
+    statstable.modify( st, same_payer, [&]( auto& s ) {
+       s.supply -= quantity;
+    });
 
     sub_balance( st.issuer, quantity );
-  }
+}
 
-  /*!
-    Retire token
-    Reduces the total quantity of tokens available by removing them from the issuer
-  */
-  void token::burn( eosio::name owner, eosio::asset quantity, string memo )
-  {
-    auto sym = quantity.symbol;
-    eosio::check( sym.is_valid(), "invalid symbol eosio::name" );
-    eosio::check( memo.size() <= 256, "memo has more than 256 bytes" );
-
-    stats statstable( _self, sym.code().raw() );
-    auto existing = statstable.find( sym.code().raw() );
-    eosio::check( existing != statstable.end(), "token with symbol does not exist" );
-    const auto& st = *existing;
-
-    require_auth( owner );
-    eosio::check( quantity.is_valid(), "invalid quantity" );
-    eosio::check( quantity.amount > 0, "must burn positive quantity" );
-
-    eosio::check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
-
-    statstable.modify( st, eosio::same_payer, [&]( auto& s ) {
-        s.supply -= quantity;
-        s.max_supply -= quantity;
-        });
-
-    sub_balance( owner, quantity );
-  }
-
-  /*!
-    Transfer token
-  */
-  void token::transfer( eosio::name    from,
-      eosio::name    to,
-      eosio::asset   quantity,
-      string  memo )
-  {
-    eosio::check( from != to, "cannot transfer to self" );
+void token::transfer( name    from,
+                      name    to,
+                      asset   quantity,
+                      string  memo )
+{
+    check( from != to, "cannot transfer to self" );
     require_auth( from );
-    eosio::check( is_account( to ), "to account does not exist");
+    check( is_account( to ), "to account does not exist");
     auto sym = quantity.symbol.code();
     stats statstable( _self, sym.raw() );
     const auto& st = statstable.get( sym.raw() );
@@ -182,182 +99,72 @@ namespace chintai
     require_recipient( from );
     require_recipient( to );
 
-    eosio::check( quantity.is_valid(), "invalid quantity" );
-    eosio::check( quantity.amount > 0, "must transfer positive quantity" );
-    eosio::check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
-    eosio::check( memo.size() <= 256, "memo has more than 256 bytes" );
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must transfer positive quantity" );
+    check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
 
     auto payer = has_auth( to ) ? to : from;
 
     sub_balance( from, quantity );
     add_balance( to, quantity, payer );
-  }
+}
 
-  /*!
-    Stake tokens
-  */
-  void token::stake( eosio::name owner, eosio::asset quantity)
-  {
-    auto sym = quantity.symbol;
-    eosio::print(sym,"\n");
-    eosio::print(sym.raw(),"\n");
-    eosio::check( sym.is_valid(), "invalid symbol eosio::name" );
+void token::sub_balance( name owner, asset value ) {
+   accounts from_acnts( _self, owner.value );
 
-    stats statstable( _self, sym.code().raw() );
-    eosio::print("Accessing stats table\n");
-    const auto& st = statstable.find( sym.code().raw() );
-    eosio::check(st != statstable.end(), "Currency does not exist");
-    eosio::print("Accessed stats table\n");
+   const auto& from = from_acnts.get( value.symbol.code().raw(), "no balance object found" );
+   check( from.balance.amount >= value.amount, "overdrawn balance" );
 
-    eosio::check( quantity.is_valid(), "invalid quantity" );
-    eosio::check( quantity.amount > 0, "must transfer positive quantity" );
-    print(quantity.symbol,"\n");
-    eosio::check( quantity.symbol == st->supply.symbol, "symbol precision mismatch" );
+   from_acnts.modify( from, owner, [&]( auto& a ) {
+         a.balance -= value;
+      });
+}
 
-    accounts from_acnts( _self, owner.value );
-    eosio::print("Accessing accounts table\n");
-    const auto& from = from_acnts.get( quantity.symbol.code().raw(), "no balance object found" );
-    eosio::check( from.balance.amount >= quantity.amount, "overdrawn balance" );
-    from_acnts.modify(from, eosio::same_payer, [&](auto & entry){
-        eosio::print("Adding ",quantity," to ",entry.balance,"\n");
-        eosio::print("Adding ",quantity," to ",entry.staked,"\n");
-        entry.balance -= quantity;
-        entry.staked += quantity;
-        });
-
-    staked_table _staked_table(_self, owner.value);
-    _staked_table.emplace(owner, [&](auto & entry){
-        eosio::print("Adding ",quantity," to ",entry.balance,"\n");
-         entry.balance = quantity;
-        });
-  }
-
-  /*!
-    Unstake tokens
-  */
-  void token::unstake( eosio::name owner, eosio::asset quantity)
-  {
-    auto sym = quantity.symbol;
-    eosio::check( sym.is_valid(), "invalid symbol eosio::name" );
-    stats statstable( _self, sym.code().raw() );
-    const auto& st = statstable.get( sym.code().raw() );
-
-    eosio::check( quantity.is_valid(), "invalid quantity" );
-    eosio::check( quantity.amount > 0, "must transfer positive quantity" );
-    eosio::check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
-
-    accounts from_acnts( _self, owner.value );
-    const auto& from = from_acnts.get( quantity.symbol.code().raw(), "no balance object found" );
-    eosio::check( from.staked.amount >= quantity.amount, "Can't unstake more than is staked" );
-    from_acnts.modify(from_acnts.find(quantity.symbol.code().raw()), eosio::same_payer, [&](auto & entry){
-        entry.staked -= quantity;
-        entry.unstaking += quantity;
-        });
-
-    unstaked_table _unstaked_table(_self, owner.value);
-    _unstaked_table.emplace(owner, [&](auto & entry){
-         entry.balance = quantity;
-        });
-
-    staked_table _staked_table(_self, owner.value);
-    bool finished(false);
-    while(_staked_table.begin() != _staked_table.end() && !finished)
-    {
-      auto i = _staked_table.rbegin();
-      if(i->balance < quantity)
-      {
-        quantity -= i->balance;
-        _staked_table.erase(_staked_table.find(i->primary_key()));
-      }
-      else
-      {
-        _staked_table.modify(_staked_table.find(i->primary_key()), owner, [&](auto & entry){
-            entry.balance -= quantity;
-            });
-        finished = true;
-      }
-    }
-  }
-
-  /*!
-    Subtract token balance from an account
-  */
-  void token::sub_balance( eosio::name owner, eosio::asset value ) {
-    accounts from_acnts( _self, owner.value );
-
-    const auto& from = from_acnts.get( value.symbol.code().raw(), "no balance object found" );
-    eosio::check( from.balance.amount >= value.amount, "overdrawn balance" );
-
-    from_acnts.modify( from, owner, [&]( auto& a ) {
-        a.balance -= value;
-        });
-  }
-
-  /*!
-    Add token balance to an account
-  */
-  void token::add_balance( eosio::name owner, eosio::asset value, eosio::name ram_payer )
-  {
-    eosio::print("37\n");
-    accounts to_acnts( _self, owner.value );
-    eosio::print("38\n");
-    auto to = to_acnts.find( value.symbol.code().raw() );
-    eosio::print("39\n");
-    if( to == to_acnts.end() ) {
-    eosio::print("40\n");
+void token::add_balance( name owner, asset value, name ram_payer )
+{
+   accounts to_acnts( _self, owner.value );
+   auto to = to_acnts.find( value.symbol.code().raw() );
+   if( to == to_acnts.end() ) {
       to_acnts.emplace( ram_payer, [&]( auto& a ){
-    eosio::print("41\n");
-          a.balance = value;
-    eosio::print("42\n");
-          });
-    eosio::print("43\n");
-    } else {
-    eosio::print("44\n");
-      to_acnts.modify( to, eosio::same_payer, [&]( auto& a ) {
-    eosio::print("45\n");
-          a.balance += value;
-    eosio::print("46\n");
-          });
-    eosio::print("47\n");
-    }
-    eosio::print("48\n");
-  }
+        a.balance = value;
+      });
+   } else {
+      to_acnts.modify( to, same_payer, [&]( auto& a ) {
+        a.balance += value;
+      });
+   }
+}
 
-  /*!
-    Create a new token balance in table
-  */
-  void token::open( eosio::name owner, const eosio::symbol& symbol, eosio::name ram_payer )
-  {
-    require_auth( ram_payer );
+void token::open( name owner, const symbol& symbol, name ram_payer )
+{
+   require_auth( ram_payer );
 
-    auto sym_code_raw = symbol.code().raw();
+   auto sym_code_raw = symbol.code().raw();
 
-    stats statstable( _self, sym_code_raw );
-    const auto& st = statstable.get( sym_code_raw, "symbol does not exist" );
-    eosio::check( st.supply.symbol == symbol, "symbol precision mismatch" );
+   stats statstable( _self, sym_code_raw );
+   const auto& st = statstable.get( sym_code_raw, "symbol does not exist" );
+   check( st.supply.symbol == symbol, "symbol precision mismatch" );
 
-    accounts acnts( _self, owner.value );
-    auto it = acnts.find( sym_code_raw );
-    if( it == acnts.end() ) {
+   accounts acnts( _self, owner.value );
+   auto it = acnts.find( sym_code_raw );
+   if( it == acnts.end() ) {
       acnts.emplace( ram_payer, [&]( auto& a ){
-          a.balance = eosio::asset{0, symbol};
-          });
-    }
-  }
+        a.balance = asset{0, symbol};
+      });
+   }
+}
 
-  /*!
-    Remove a token balance in table
-  */
-  void token::close( eosio::name owner, const eosio::symbol& symbol )
-  {
-    require_auth( owner );
-    accounts acnts( _self, owner.value );
-    auto it = acnts.find( symbol.code().raw() );
-    eosio::check( it != acnts.end(), "Balance row already deleted or never existed. Action won't have any effect." );
-    eosio::check( it->balance.amount == 0, "Cannot close because the balance is not zero." );
-    acnts.erase( it );
-  }
+void token::close( name owner, const symbol& symbol )
+{
+   require_auth( owner );
+   accounts acnts( _self, owner.value );
+   auto it = acnts.find( symbol.code().raw() );
+   check( it != acnts.end(), "Balance row already deleted or never existed. Action won't have any effect." );
+   check( it->balance.amount == 0, "Cannot close because the balance is not zero." );
+   acnts.erase( it );
+}
 
-} 
+} /// namespace eosio
 
-EOSIO_DISPATCH( chintai::token , (create)(issue)(transfer)(open)(close)(retire)(burn)(stake)(unstake) )
+EOSIO_DISPATCH( eosio::token, (create)(issue)(transfer)(open)(close)(retire) )
